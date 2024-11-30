ad617baf3043387a5c536346ddc89921e506db23
Rune Soerensen <rsoerensen@salesforce.com>
2024-11-30 06:05:26 -0500

    Initial implementation (#14)

---
+++ lib/output.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This is technically redundant, since all consumers of this lib will have enabled these,
+# however, it helps Shellcheck realise the options under which these functions will run.
+set -euo pipefail
+
+ANSI_RED='\033[1;31m'
+ANSI_RESET='\033[0m'
+
+# Output a styled multi-line error message to stderr.
+#
+# Usage:
+# ```
+# output::error <<-EOF
+# 	Error: The error summary.
+#
+# 	Detailed description.
+# EOF
+# ```
+function output::error() {
+	echo >&2
+	while IFS= read -r line; do
+		echo -e "${ANSI_RED} !     ${line}${ANSI_RESET}" >&2
+	done
+	echo >&2
+}
---
+++ spec/hatchet/buildpack_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+
+RSpec.describe 'Buildpack execution' do
+  context 'when building a valid .NET app' do
+    let(:app) { Hatchet::Runner.new('spec/fixtures/basic_web_8.0') }
+
+    it 'successfully compiles' do
+      app.deploy do |app|
+        expect(clean_output(app.output)).to match(Regexp.new(<<~REGEX))
+          remote: -----> Using buildpack: #{DEFAULT_BUILDPACK_URL}
+          remote: -----> .NET app detected
+          remote: -----> SDK version detection
+          remote:        Detected .NET file to publish: `/tmp/build_.*/foo.csproj`
+          remote:        Inferring version requirement from `/tmp/build_.*/foo.csproj`
+          remote:        Detected version requirement: .*
+          remote:        Resolved .NET SDK version .*
+          remote: -----> SDK installation
+          remote:        Downloading SDK from .*
+          remote:        Verifying SDK checksum
+          remote:        Installing SDK
+          remote: -----> Publish solution
+          remote:        Using `Release` build configuration
+          remote:        Running `dotnet publish /tmp/build_.*/foo.csproj --runtime linux-x64 "-p:PublishDir=bin/publish"`
+          remote: 
+          remote:            Determining projects to restore...
+          remote:            Restored /tmp/build_.*/foo.csproj .*
+          remote:            foo -> /tmp/build_.*/bin/Release/net8.0/linux-x64/foo.dll
+          remote:            foo -> /tmp/build_.*/bin/publish/
+          remote: 
+          remote:        Done .*
+          remote: -----> Setting launch table
+          remote:        Detecting process types from published artifacts
+          remote:        Added `foo`: .*
+          remote: -----> Done .*
+          remote: -----> Discovering process types
+          remote:        Procfile declares types     -> \\(none\\)
+          remote:        Default types for buildpack -> foo
+        REGEX
+      end
+    end
+  end
+
+  context 'when building an app with MSBUILD_VERBOSITY_LEVEL=invalid' do
+    let(:app) do
+      Hatchet::Runner.new(
+        'spec/fixtures/basic_web_8.0',
+        config: { 'MSBUILD_VERBOSITY_LEVEL' => 'invalid' },
+        allow_failure: true
+      )
+    end
+
+    it 'fails with error from CNB' do
+      app.deploy do |app|
+        expect(clean_output(app.output)).to include(<<~OUTPUT)
+          remote:  !     Invalid MSBuild verbosity level
+          remote:  !    
+          remote:  !     The `MSBUILD_VERBOSITY_LEVEL` environment variable value (`invalid`)
+          remote:  !     is invalid. Did you mean one of the following supported values?
+          remote:  !    
+          remote:  !     d
+          remote:  !     detailed
+          remote:  !     diag
+          remote:  !     diagnostic
+          remote:  !     m
+          remote:  !     minimal
+          remote:  !     n
+          remote:  !     normal
+          remote:  !     q
+          remote:  !     quiet
+        OUTPUT
+      end
+    end
+  end
+end
