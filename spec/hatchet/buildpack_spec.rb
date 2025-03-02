89bef75aba12abeb26f2852bcaced632b6a21c9b
Rune Soerensen <rsoerensen@salesforce.com>
2025-03-02 00:20:53 -0500

    Verify .NET runtime dependencies

--- spec/hatchet/buildpack_spec.rb
+++ spec/hatchet/buildpack_spec.rb
@@ -40,6 +40,8 @@ RSpec.describe 'Buildpack execution' do
           remote:        Procfile declares types     -> \\(none\\)
           remote:        Default types for buildpack -> foo
         REGEX
+
+        expect(app.run('bin/test-runtime.sh')).to match('All dynamically linked libraries were found.')
       end
     end
   end
