bc85f361a94edbfea43b0fb7f734d545473a3710
Rune Soerensen <rsoerensen@salesforce.com>
2025-03-13 02:00:32 -0400

    Refactor launch process parsing
    Extract shareable logic to separate file

---
+++ lib/cnb_process_utils.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+parse_launch_processes() {
+	local -n _processes=$1
+	local launch_toml_file="${2}"
+
+	local type=""
+	local command=""
+
+	while IFS= read -r line; do
+		# Extract the type from lines like `type = "web"`
+		if [[ ${line} =~ ^type[[:space:]]*=[[:space:]]*\"([^\"]+)\" ]]; then
+			type="${BASH_REMATCH[1]}"
+		fi
+
+		# Extract the command from lines like `command = ["bash", "-c", "run_command"]`
+		if [[ ${line} =~ ^command[[:space:]]*=[[:space:]]*\[\"bash\",[[:space:]]*\"-c\",[[:space:]]*\"(.*)\"[[:space:]]*\] ]]; then
+			command="${BASH_REMATCH[1]}"
+			_processes["${type}"]="${command}"
+			type=""
+			command=""
+		fi
+	done <"${launch_toml_file}"
+}
